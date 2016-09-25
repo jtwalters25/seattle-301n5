5dd408d01fb880e693c70fa94f2d23dafa7eeebf
Jeremiah <jeremiaht.walters@gmail.com>
2016-09-25 14:58:55 -0700

    hid tabs

--- class-03-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
+++ class-03-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
@@ -48,12 +48,16 @@ articleView.handleCategoryFilter = function() {
 
 articleView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
+    var $whereToGo = $(this).data('content');
+    $('.tab-content').hide();
+    $('#' + $whereToGo).fadeIn();
+  })
     /* TODO:
       1. Hide all of the .tab-content sections
       2. Fade in the single .tab-content section that is
         associated with the .tab element's data-content attribute.
     */
-  });
+  ;
   $('.main-nav .tab:first').click();
 };
 
@@ -78,3 +82,4 @@ articleView.setTeasers = function() {
 articleView.populateFilters();
 articleView.handleAuthorFilter();
 articleView.handleCategoryFilter();
+articleView.handleMainNav ();
