66e2ea51d0a2e3606b160932941c5c2751bec3e3
Jeremiah <jeremiaht.walters@gmail.com>
2016-10-27 20:57:03 -0700

    really working

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/aboutController.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/aboutController.js
@@ -7,8 +7,7 @@
     // REVIEW: Look at this method chaning. What is being accomplished here?
     $('#about').show().siblings().hide();
     repos.requestRepos(repoView.index);  // TODOne: Call a function to 'request' our repo data.
-    });// Pass in a view function as a callback, so our repos will render after the data is loaded.
-  };
+  };// Pass in a view function as a callback, so our repos will render after the data is loaded.
 
   module.aboutController = aboutController;
 })(window);
