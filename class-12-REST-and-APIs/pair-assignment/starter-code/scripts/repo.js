5f6d409f8187657843c167dc6a6138d914acd0a2
Jeremiah <jeremiaht.walters@gmail.com>
2016-10-27 20:50:50 -0700

    working

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
@@ -14,27 +14,19 @@
         Authorization: 'token ' + githubToken
       }
     }).done(function(data) {
-      console.log(data)
-      data.filter(function(ele) {
-        return ele.owner === 'jtwalters25';
-      }).forEach(function(repo) {
-        var output = '<h2>' + repo.name + '</h2>' +
-          '<p>' + repo.description + '</p>';
-        $('#repo-template').append(output);
-      })
+      repos.all = data
+      callback();
     });
-    callback();
-  };
 
 
-  // DONE: Model method that filters the full collection for repos with a particular attribute.
-  // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
-  repos.with = function(attr) {
-    return repos.all.filter(function(repo) {
-      return repo[attr];
-    });
-  };
-
+    // DONE: Model method that filters the full collection for repos with a particular attribute.
+    // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
+    repos.with = function(attr) {
+      return repos.all.filter(function(repo) {
+        return repo[attr];
+      });
+    };
+  }
 
   module.repos = repos;
 })(window);
--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repoView.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repoView.js
@@ -10,24 +10,20 @@
     $about.find('ul').empty();
     $about.show().siblings().hide();
   };
-  var render = function(repo) {
-    var source = $('#repo-template').html();
-    var template = Handlebars.compile(source);
+
+  var source = $('#repo-template').html();
+  var render = Handlebars.compile(source);
     // TODO: Remember that new Handlebars template? Let's compile it!
     // Save the result in this `render` variable.
-    return template(repo);
-  };
+
   // DONE: If all the data is loaded, we can prep the UI and render the repos.
-  repoView.index = function(render) {
-    ui(render);
+  repoView.index = function() {
+    ui();
 
     // The jQuery `append` method lets us append an entire array of HTML elements at once,
-    // So we can use a little FP to transform our data-set into DOM nodes:
-console.log(repos.with('name'))
+
     $('#about ul').append(
-  repos.with('name').forEach(function(repo){
-    render(repo);
-  })
+  repos.with('name').map(render)
     );
   };
 
